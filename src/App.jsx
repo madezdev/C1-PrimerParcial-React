775c4b9e2997b0471ca4eb8ef96f0eaf46592305
Martin Hernandez <madez@MacBook.local>
2023-10-30 20:35:08 -0300

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+import Card from './components/Card/Card'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Carga de estudiantes</h1>
+      <form>
+        <label htmlFor="name">Nombre </label>
+      </form>
+      <Card/>
+    </>
+  )
+}
+
+export default App
